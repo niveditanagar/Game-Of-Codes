be90d7a58356e4ef21a62f90931cac8ba5586ad3
Daniel Zorrilla <danezorrilla@gmail.com>
2018-08-09 12:53:50 -0400

    refactor

---
+++ README.md
@@ -0,0 +1,25 @@
+# Codet ReadMe
++ We created this app for Development and Programming lovers!
++ Type in your Email and create a Password and then Sign-up.
++ Creating your login credentials will push that information to a database so that your information will be stored for later use.
++ Already an existing user?  Enter your email and password  and then click the Log-in button.
++ Once Logged in, you will be directed to your personal account's home page.
++ On this page, you will be able to make a post and that  post will be shown directly above all of your previous posts to the site.
++ Each one of your posts will be stored in the same database as your login and password.
+
+## Built With:
+* html
+* Handlebars
+* Sequalize
+* MySQL
+* Express
+* Semantic UI style link
+* Quill.js
+* external css style sheet
+* external javascript files
+
+## Authors:
+* Patrick Joyce
+* Daniel Z
+* Nivi Nagar
+* Marlee
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,19 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    db.User.findAll({}).then(function(dbUser){
+      res.render("index");
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
   });
 
   app.get("/home/:Email", function(req, res){
@@ -27,70 +21,6 @@ module.exports = function(app) {
       res.render("home", {email: dbUser.Email, content: dbUser.PostContents});
     });
   });
-
-  app.get("/home/:Email/:Password", function(req, res){
-    console.log(req.params.Email);
-
-    console.log(req.params.Password);
-    bcrypt.genSalt(10, function(err, salt){
-      bcrypt.hash(req.params.Password, salt, function(err, hash){
-        req.params.Password = hash;
-        encp = req.params.Password;
-
-        console.log("Enc", encp);
-        db.User.findOne({where: { Email: req.params.Email}, $and: { Password: encp} }).then(function(dbUser){
-          console.log(dbUser);
-          if(!dbUser){
-            res.render("404");
-          } else if(!dbUser.Password){
-            res.render("403");
-          } else{
-            res.render("home", {email: dbUser.Email});
-            // res.json(dbUser);
-          }
-
-
-
-
-        });
-      });
-    });
-
-    // db.User.findOne({ where: { Email: req.params.Email }, include: [db.PostContent], order:[[db.PostContent,"createdAt", 'DESC']] }).then(function(dbUser){
-    //   console.log(dbUser);
-    //   if(!dbUser){
-    //     res.render("404");
-    //   } else{
-    //     res.render("home", {email: dbUser.Email, content: dbUser.PostContents});
-    //     res.json(dbUser);
-
-    //   }
-
-    // });
-
-  });
-
-  app.get("/home/:Email/profile", function(req, res){
-    db.User.findOne({}).then(function(dbUser){
-      res.render("profile");
-    });
-  });
-
-  // app.get("/home/:Email/api/content", function(req,res){
-  //   db.postContent.findAll({}).then(function(data){
-  //     res.render("home", { content: data } );
-  //   })
-  // })
-
-  // app.get("/home/:Email", function(req, res){
-  //   var email = req.params.Email;
-  //   //console.log("Email", email);
-  //   db.User.findOne({ where: { Email: email } }).then(function(dbUser){
-  //     res.render("home", {
-  //       Email: dbUser
-  //     });
-  //   });
-  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
